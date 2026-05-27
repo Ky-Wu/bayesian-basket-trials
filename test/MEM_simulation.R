823b9dd101ca5ce19699f53ac95c76e61ac9616c
Ky-Wu <kylelinwu@gmail.com>
2026-05-27 16:15:23 -0700

    add comments

--- test/MEM_simulation.R
+++ test/MEM_simulation.R
@@ -1,13 +1,24 @@
+# Finding MEM optimal design
+# Evaluation is handled in a separate file, eval_MEM_simulation.R
+
+# load libraries
+library(basket)
 library(data.table)
 rm(list = ls())
 set.seed(1130)
 
+# load helper functions
 source(file.path(getwd(), "src", "sim_helper.R"))
 source(file.path(getwd(), "src", "MEM.R"))
 
+# load command line args:
+# sim_i: index of param grid to evaluate
+# This is meant for a script that loops through the indices (1:112)
+# of the param grid for parallel evaluation.
 args <- commandArgs(trailingOnly = TRUE)
 sim_i <- as.integer(args[1])
 
+# Scenarios to evaluate
 scenarios <- data.frame(
   "Global_Null" = c(0.05, 0.05, 0.05, 0.05),
   "Global_Alternative" = c(0.2, 0.2, 0.2, 0.2),
@@ -18,30 +29,36 @@ scenarios <- data.frame(
   "Half" = c(0.2, 0.2, 0.05, 0.05)
 )
 
+# Number of simulations to use
 n_sim <- 200
 p0 <- 0.05
 p1 <- 0.20
+# second-stage Type I and II error constraints
+# (under global null and alternative, respectively)
 alpha2 <- 0.05
 beta2 <- 0.20
+# Parameter grid to evaluate
+# alpha1: first-stage Type I error constraint
+# beta1: first-stage Type II error constraint
 param_grid <- expand.grid(alpha1 = seq(0.10, 0.45, by = 0.05),
                           beta1 = seq(0.02, 0.15, by = 0.01))
-# MEM method
 MEM_param_grid <- param_grid
 MEM_param_grid$ESS <- 0
 cat("Calibrating MEM two-stage design...")
 alpha1 <- MEM_param_grid[sim_i,]$alpha1
 beta1 <- MEM_param_grid[sim_i,]$beta1
-#cat("alpha1: ", alpha1, "| beta1: ", beta1, "\n")
+# Calibrate MEM design and evaluate ESS under setting of (alpha1, beta1)
 cal_time <- system.time({
-  calibrate <- calibrateTwoStage(B = 4, p0 = 0.05, p1 = 0.20,
+  calibrate <- calibrateTwoStage(B = 4, p0 = p0, p1 = p1,
                                  alpha1 = alpha1, beta1 = beta1,
                                  alpha2 = alpha2, beta2 = beta2, n_sim = n_sim,
                                  MEMEfficacy)
 })
 
+# Save estimated ESS in data frame
 res <- data.frame(alpha1 = alpha1, beta1 = beta1,
                   ESS = calibrate$ESS, cal_time = cal_time[[3]])
-
+# save result
 name <- paste0("MEM_sim", sim_i, ".csv")
 dir.create(file.path(getwd(), "output", "MEM_simulation"), showWarnings = FALSE)
 fwrite(res, file.path(getwd(), "output", "MEM_simulation", name))
--- test/eval_MEM_simulation.R
+++ test/eval_MEM_simulation.R
@@ -1,8 +1,13 @@
+# Evaluation of MEM optimal design
+
+# load libraries and helper functions
+library(basket)
 library(data.table)
 set.seed(1130)
 source(file.path(getwd(), "src", "sim_helper.R"))
 source(file.path(getwd(), "src", "MEM.R"))
 
+# scenarios to evaluate
 scenarios <- data.frame(
   "Global_Null" = c(0.05, 0.05, 0.05, 0.05),
   "Global_Alternative" = c(0.2, 0.2, 0.2, 0.2),
@@ -12,33 +17,44 @@ scenarios <- data.frame(
   "Bad_Nugget" = c(0.2, 0.05, 0.05, 0.05),
   "Half" = c(0.2, 0.2, 0.05, 0.05)
 )
+# Calibration and evaluation of MEM designs are handled in MEM_simulation.R
+
+# extract saved results
 files <- dir(file.path(getwd(), "output", "MEM_simulation"))
 param_grid <- data.frame()
 for (file in files) {
   param_grid <- rbind(param_grid,
                       fread(file.path(getwd(), "output", "MEM_simulation", file)))
 }
+# find optimal MEM design
 alpha1 <- param_grid[which.min(param_grid$ESS),]$alpha1
 beta1 <- param_grid[which.min(param_grid$ESS),]$beta1
+
+# historical/control response rate
 p0 <- 0.05
+# minimal clinically meaningful response rate
 p1 <- 0.20
+# second-stage type I error rate constraint under global null
 alpha2 <- 0.05
+# second-stage type II error rate constraint under global alternative
 beta2 <- 0.20
+# number of simulated trials to use in each scenario
 n_sim <- 200
+# calibrate design: find required sample sizes and stopping rules
 cal_time <- system.time({
-  MEM_calibrate <- calibrateTwoStage(B = 4, p0 = 0.05, p1 = 0.20,
+  MEM_calibrate <- calibrateTwoStage(B = 4, p0 = p0, p1 = p1,
                                  alpha1 = alpha1, beta1 = beta1,
                                  alpha2 = alpha2, beta2 = beta2, n_sim = n_sim,
                                  MEMEfficacy, increment_start = 13)
 })
-
+# evaluate final design in each scenario
 system.time({
   MEM_res <- evaluateTwoStageScenarios(MEM_calibrate$n_i,
                                       MEM_calibrate$n_b,
-                                      scenarios, p0 = 0.05,
+                                      scenarios, p0 = p0,
                                       MEM_calibrate$interim_threshold,
                                       MEM_calibrate$pp_threshold,
                                       MEMEfficacy, n_sim = n_sim)
 })
-
+# save results
 saveRDS(MEM_res, file.path(getwd(), "output", "twostage_comparison", "MEM_0.1.rds"))
